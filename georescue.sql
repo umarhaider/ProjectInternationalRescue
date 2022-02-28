f1957a9c60b75b748cc2b6856c0edab1b550341f
umarhaider <74437199+umarhaider@users.noreply.github.com>
2022-02-28 10:10:19 +0000

    Add files via upload

--- georescue.sql
+++ georescue.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2022 at 10:35 AM
+-- Generation Time: Feb 28, 2022 at 11:09 AM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.1
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `georescue` (
   `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `text` varchar(255) NOT NULL,
   `longitude` int(255) NOT NULL,
   `latitude` int(255) NOT NULL,
