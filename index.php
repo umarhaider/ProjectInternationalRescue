79d89ed1ad20b12d0ddcc6ff3a4e853a2908d98f
Nathan Hannah <74415841+nathanhannah122@users.noreply.github.com>
2022-02-28 19:54:55 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,290 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Geo Rescue</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  
-  <!-- JavaScript -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-
-  <!-- mdb -->
-  <link rel="stylesheet" href="mdb.min.css" />
-  
-  <!-- JQuery -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script>
-
-  <!-- Leafly-->
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-   crossorigin=""/>
-
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-   crossorigin="">
-  </script>
-   <!-- Font Awesome -->
-  <link href="assets/vendor/fontawesome/css/fontawesome.min.css" rel="stylesheet">
-  <link href="assets/vendor/fontawesome/css/solid.min.css" rel="stylesheet">
-  <link href="assets/vendor/fontawesome/css/brands.min.css" rel="stylesheet">
-
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<!-- NAVIGATION BAR -->
-<div class="navigationbar">
-<nav class="navbar navbar-expand-lg navbar navbar-custom">
-  <img src="geoRescue.jpg" class="rounded float-left img" alt="Logo">
-  <p style="margin-left: 5px;"class="mb-0">Geo Rescue</p>
-</nav>
-</div>
-
 <?php
-  session_start();
-  // Server login details
-  include 'conn.php';
-  // SQL Get all users
-  $sql = "SELECT * FROM georescue";
-   
-  // try to connect and send SQL query with details
-  // if unable return error 
-  try{
-   $pdo = new PDO($dsn, $username, $password);
-   $stmt = $pdo->query($sql);
-   $stmt2 = $pdo->query($sql);
-   if($stmt === false){
-    die("Error");
+   // Start the session
+   session_start();
+
+   $_SESSION['name'] = $_POST['name'];
+   $_SESSION['email'] = $_POST['email'];
+   $_SESSION['age']  = $_POST['age'];
+
+   // check if strings are empty
+   if (empty($_POST["email"]) || empty($_POST["password"]) || empty($_POST["name"]) || empty($_POST["age"])) {
+      $_SESSION['errMessage'] = 'Please enter all fileds';
+      header('location: index.php');
+   } 
+
+   // post data from signup form
+   $email      = checkData($_POST['email']);
+   $password   = checkData($_POST['password']);
+   $HashedPass = hash('sha512', $password.SALT_STRING);
+   $name       = checkData($_POST['name']);
+   $age        = checkData($_POST['age']);
+
+   $nameCheck  = checkName($name);
+   $ageCheck   = validateAge($age);
+
+   if ($nameCheck == True) {
+      $_SESSION['errMessage'] = 'Only Alphabetic chararcters for the name field';
+      header('location: index.php');
+      exit();
+   } elseif ($ageCheck == False) {
+      $_SESSION['errMessage'] = 'Invalid Age';
+      header('location: index.php');
+      exit();
+   }
+
+   // calls function to input to DB  
+   processData($name, $email, $age, $HashedPass);
+
+   // sanitises and returns data
+   function checkData($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
    }
    
-  }catch (PDOException $e){
-    echo $e->getMessage();
-  }
-?>
-
-
-
-<!-- Error Alerts -->
-<?php
-  // if signup process fails, output error message
-  if (isset($_SESSION['errorMessage'])) {
-  //echo $_SESSION['login_error_msg'];
-  echo "<div id='errortext' class='alert alert-danger alert-dismissible fade show' role='alert'>";
-  echo "<strong>Error! </strong>";
-  echo $_SESSION['errorMessage'];
-  unset($_SESSION['errorMessage']);
-  echo  "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
-  echo "<span aria-hidden='true'>&times;</span></div>";
-  }
-
-?>
-
-
-<?php
-  // if signup process fails, output error message
-  if (isset($_SESSION['successMessage'])) {
-  //echo $_SESSION['login_error_msg'];
-  echo "<div id='successtext' class='alert alert-success alert-dismissible fade show' role='alert'>";
-  echo "<strong>Success! </strong>";
-  echo $_SESSION['successMessage'];
-  unset($_SESSION['successMessage']);
-  echo  "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
-  echo "<span aria-hidden='true'>&times;</span></div>";
-  }
-
-?>
-<div id="map"></div>
-<div id="location"></div>
-<button onclick="getLocation()">Current Location</button>
-
-<!-- MAIN CONTENT - TABLE -->
-<div class="container ">
-  <div class="row">
-    <div class="col">
-      <h2 style="text-align: center; margin-top: 20px;">Pinpoints</h2>
-      <table id="dtBasicExample" width="100%" class="table table-hover" cellspacing="0" >
-        <thead>
-          <tr>
-            <th class="th-sm">Longitude
-            </th>
-            <th class="th-sm">Latitude
-            </th>
-            <th class="th-sm">Details
-            </th>
-            <th class="th-sm">Person
-            </th>
-            </th>
-            <th class="th-sm">Priority
-            </th>
-            <th class="th-sm">Status
-            </th>
-            <th class="th-sm">Action
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-        <?php while($row = $stmt2->fetch(PDO::FETCH_ASSOC)) : ?>
-        <tr>
-           <td><?php echo htmlspecialchars($row['longitude']); ?></td>
-           <td><?php echo htmlspecialchars($row['latitude']); ?></td>
-           <td><?php echo htmlspecialchars($row['details']); ?></td>
-           <td><?php echo htmlspecialchars($row['name']); ?></td>
-           <td> <?php if ($row['priority'] == 'low') {
-              echo '<span class="low">' . htmlspecialchars($row['priority']) . '<span>'; 
-            } else if ($row['priority'] == 'medium') {
-              echo '<span class="medium">' . htmlspecialchars($row['priority']) . '<span>'; 
-            } else {
-              echo '<span class="high">' . htmlspecialchars($row['priority']) . '<span>'; 
-            } ?>
-            </td>
-           <td><?php echo htmlspecialchars($row['status']); ?></td>
-           <td><a href="action.php?action=updateStatus&item=<?php echo htmlspecialchars($row['id']); ?>">
-            <button class="btn btn-success" <?php if($row['status'] == 'Complete'){ echo 'disabled'; } ?> >Complete</button></a><br><br>
-            <a href="action.php?action=deleteItem&item=<?php echo htmlspecialchars($row['id']); ?>"><button class="btn btn-danger"><i class="fa-trash-can"></i>Delete</button><i class="fa-solid fa-trash-can"></i></a>
-
-            <a onclick="showMarker(<?php echo htmlspecialchars($row['latitude']);?>, <?php echo htmlspecialchars($row['longitude']);?>)" class="btn btn-secondary"></i>View</a> 
-
-        </tr>
-        <?php endwhile; ?>
-        </tbody>
-        </div>
-      </table>
-    </div>
-    <div class="col form" style="margin-top: 25px;">
-        <!-- form login -->
-        <form class="" method="post" action="form-action.php">
-            <!-- Send Screen Details -->
-            <span class="h4 mb-4 today" title="Today"></span>
-            <p class="h4 mb-4">Enter Pinpoint details - click on map to select area </i></p>
-
-            <!-- Location Latitude-->
-            <input type="text" name="lat" value="" class="form-control mb-4" placeholder="Latitude" id="lat" required>
-
-            <!-- Location Longitude -->
-            <input type="text" name="long" value="" class="form-control mb-4" placeholder="Longitude" id="long" required>
-
-            <!-- name -->
-            <input type="text" name="name" class="form-control mb-4" placeholder="Name" id="name" required maxlength="40">
-
-            <!-- Description -->
-            <input type="text" name="details" class="form-control mb-4" placeholder="Details" id="details" maxlength="100" required>
-
-            <!-- Priority -->
-            <select class="form-control mb-4" name="priority" id="priority">
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
-            </select>
-
-            <!-- Submit button -->
-            <button class="btn btn-info btn-block my-4" style="background-color: #4723D9;"type="submit">Submit</button>
-
-        </form>
-    </div>
-
-  </div>
-
-
-<script type="text/javascript">
-  var x = document.getElementById("location");
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+   // checks name input for alphabetic chars
+   function checkName($data){
+   if (!preg_match("/^[a-zA-Z ]*$/",$data)) {  
+      return True;
+      }  
+   return False;
+   }
+   
+   // validates user age, numerical, max and min age
+   function validateAge($age) {
+    if (preg_match("/^[0-9]+$/", $age)) {
+        return true;  
+    } else if ($age >= 120){
+        return false;
+    } else if ($age < 1){
+      return false;
     }
-  }
+    return false;
+   }
 
-  function showPosition(position) {
-    map.setView([position.coords.latitude, position.coords.longitude], 16)
-    var popup = L.popup()
-    .setLatLng([position.coords.latitude, position.coords.longitude])
-    .setContent("You are here")
-    .openOn(map);
-    var outputValueLat = position.coords.latitude;
-    document.getElementById("lat").value = outputValueLat;
-    var outputValueLong = position.coords.longitude;
-    document.getElementById("long").value = outputValueLong;
-    
-  }
-</script>
+   // inputs values into database
+   // params - name, email, age and SHA 512 hashed password
+   // when successful redirects user to success message
+   function processData($name, $email, $age, $HashedPass) {
+      include 'db-conn.php';
+      try{
+          $sql = "INSERT INTO customer (name, email, age, password) VALUES (:username, :useremail, :userage, :userpassword)";
+          // Prepare statement.
+          $pdo = new PDO($dsn, $username, $passwordDb);
+          $statement = $pdo->prepare($sql);
 
-
-<!-- Enables datatable -->
-<script type="text/javascript">
-$(document).ready(function () {
-  $('#dtBasicExample').DataTable();
-  $('.dataTables_length').addClass('bs-select');
-});
-
-</script>
-
-
-<script type="text/javascript">
-var map = L.map('map').setView([51.505, -0.09], 6);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+          // Bind values to the parameter.
+          $statement->bindValue(':username', $name);
+          $statement->bindValue(':useremail', $email);
+          $statement->bindValue(':userage', $age);
+          $statement->bindValue(':userpassword', $HashedPass);
+          // Execute the statement and insertvalues.
+          $inserted = $statement->execute();
+          header('location: success.php');
+      // Catch errors
+      } catch (PDOException $e) {
+          echo $e->getMessage();
+      }
+   }
 
 
-</script>
 
-<?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
-  <script type="text/javascript">
-  var lat = "<?php echo htmlspecialchars($row['latitude']); ?>";
-  var long = "<?php echo htmlspecialchars($row['longitude']); ?>";
-  var text = "<?php echo htmlspecialchars($row['details']); ?>";
-    L.marker([lat, long]).addTo(map)
-      .bindPopup(text)
-      .openPopup();
-  var popup = L.popup();
-  </script>
-<?php endwhile; ?>
-
-
-<script type="text/javascript">
-
-function showMarker(lat, long) {
-  map.setView([lat, long], 16);
-}
-
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("At " + e.latlng.toString())
-        .openOn(map);
-}
-
-function populateForm(e) {
-    var outputValueLat = e.latlng.lat;
-    document.getElementById("lat").value = outputValueLat;
-    var outputValueLong = e.latlng.lng;
-    document.getElementById("long").value = outputValueLong;
-}
-
-map.on('click', onMapClick);
-map.on('click', populateForm);
-</script>
-
-
-<!-- FOOTER -->
-<div class="footer-copyright text-center py-3">© 2022 Copyright 
-  <a href="https://github.com/umarhaider/ProjectInternationalRescue" target="blank_"> Nathan Hannah, Umar Haider, Harriet Brooke</a>
-</div>
-
-</body>
-</html>
+?>
